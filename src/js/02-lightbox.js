52443a679bbe9e4394be763a5919f32bee789f02
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-12 21:04:06 +0200

    update 02-lightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const listImg = document.querySelector(".gallery");
+
+const createItems = galleryItems
+    .map((item) => {
+        return `
+        <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`;
+    })
+    .join("");
+
+listImg.innerHTML = createItems;
+
+var lightbox = new SimpleLightbox(".gallery");
+
 console.log(galleryItems);
