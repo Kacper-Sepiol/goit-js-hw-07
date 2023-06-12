0db142af9208e624859d0ef43df409bac4298fb6
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-12 16:42:03 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import * as basicLightbox from "basiclightbox";
+
+const listImg = document.querySelector(".gallery");
+
+galleryItems.forEach((element) => {
+    const listImgElement = document.createElement("li");
+    const divListImg = document.createElement("div");
+    const linkListImg = document.createElement("a");
+    const imgElement = document.createElement("img");
+
+    divListImg.classList.add("gallery__item");
+
+    linkListImg.classList.add("gallery__link");
+    linkListImg.setAttribute("href", element.original);
+
+    imgElement.setAttribute("data-src", element.original);
+    imgElement.setAttribute("src", element.preview);
+    imgElement.setAttribute("alt", element.description);
+    imgElement.classList.add("gallery__image");
+
+    listImg.append(listImgElement);
+    listImgElement.append(divListImg);
+    divListImg.append(linkListImg);
+    linkListImg.append(imgElement);
+});
 
 console.log(galleryItems);
+
+console.log(listImg);
